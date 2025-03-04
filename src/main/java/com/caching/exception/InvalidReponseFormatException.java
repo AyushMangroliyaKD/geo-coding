c3d3f880e46b2f9b2840c1e105aaedff458e1b62
Ayush <ayushjaysukh.mangroliya@kickdrumtech.com>
2025-03-04 11:05:21 +0530

    Actuator COde

--- src/main/java/com/caching/exception/InvalidReponseFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.caching.exception;
-
-public class InvalidReponseFormatException extends RuntimeException {
-    public InvalidReponseFormatException(String message) {
-        super(message);
-    }
-}
---
+++ src/test/java/kdu/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package kdu.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
